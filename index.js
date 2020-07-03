559536027a7f529a265a85472a82f4f6c2566822
Uno <ltung630@gmail.com>
2020-07-03 17:52:22 +0700

    - Update content

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-const helloWorld = React.createElement('h1', null, 'Hello World');
+const helloWorld = React.createElement('h1', null, 'I am from React');
 ReactDOM.render(helloWorld, document.getElementById('root'));
